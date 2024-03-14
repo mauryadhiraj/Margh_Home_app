2781246340c715470821b829380164362ab39338
Suraj Maurya <156579033+mauryasuraj730@users.noreply.github.com>
2024-03-14 17:43:39 +0530

    Initial commit

---
+++ app/src/main/java/com/data/marghapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.data.marghapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //Home page
+    }
+}
